223a1a7d3566aff92a7bd645e4cec2f466a70b33
Uzair <uzairhajra76330@gmail.com>
2021-07-28 12:43:53 +0530

    beach resort project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RoomConsumer, RoomProvider, RoomContext } from "./context";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
